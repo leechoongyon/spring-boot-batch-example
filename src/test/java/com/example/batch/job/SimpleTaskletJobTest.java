50aa158326144a936285d15ded48bea2d2c7f98c
LeeJunGyun <rainmaker0303@hanmail.net>
2021-07-01 21:39:17 +0900

    update

--- src/test/java/com/example/batch/job/SimpleTaskletJobTest.java
+++ src/test/java/com/example/batch/job/SimpleTaskletJobTest.java
@@ -7,9 +7,7 @@ import com.example.batch.repository.MemberRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +33,11 @@ public class SimpleTaskletJobTest {
 
     @Test
     public void simpleTaskletJobTest() throws Exception {
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("test", "testData123")
+                .toJobParameters();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
         Assert.assertThat(jobExecution.getStatus(),  is(BatchStatus.COMPLETED));
         Assert.assertThat(jobExecution.getExitStatus(),  is(ExitStatus.COMPLETED));
     }
